0a5f6a6ed4c9415742af673aa1d1e76b0996ae32
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-15 18:07:40 +0600

    task 50 is done

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -6,60 +6,7 @@
 // 5 9 2 3
 // 8 4 2 4
 // 1 7 -> такой позиции в массиве нет
-// void PrintArray(int[,] matr)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++) 
-// {
-//     for (int j = 0; j < matr.GetLength(1); j++) 
-//     {
-//         Console.Write($"{matr[i, j]} \t");
-//     }
-//   Console.WriteLine();
-// }
-// }
-// void FillArray(int[,] matr)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++) 
-// {
-//     for (int j = 0; j < matr.GetLength(1); j++) 
-//     {
-//         matr[i, j] = new Random().Next(1, 100);
-//     }
-// }
 
-// }
-// void FindArray(int[,] matr, int m, int n)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++) 
-// {
-//     for (int j = 0; j < matr.GetLength(1); j++) 
-//     {
-//         if (i == m && j == n)
-//         {
-//              Console.Write($"{matr[i, j]} \t");
-//         }
-//         else
-//         Console.Write(" Такой позиции нет ");
-//     }
-// }
-// }
-
-
-
-
-
-// Console.Clear();
-// Console.Write("Введите размеры матрицы: ");
-// string[] numbers = Console.ReadLine().Split(" ");
-// int[,] matrix = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
-// Console.Write("Введите искомую позицию: ");
-// string[] number = Console.ReadLine().Split(" ");
-// int m = int.Parse(number[0]);
-// int n = int.Parse(number[1]);
-// PrintArray(matrix);
-// FillArray(matrix);
-// PrintArray(matrix);
-// FindArray(matrix,m,n);
 void PrintArray(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++) 
@@ -83,6 +30,13 @@ void FillArray(int[,] matr)
 
 }
 
+void ReleaseMatrix(int[, ] matrix, int m, int n)
+{
+    if (m >= matrix.GetLength(0) || n >= matrix.GetLength(1) || m < 0 || n < 0)
+        Console.WriteLine("К сожалению, такая позиция отсуствует");
+    else
+        Console.Write($"{matrix[m - 1, n - 1]} \t");
+}
 
 
 Console.Clear();
@@ -96,8 +50,5 @@ int n = int.Parse(number[0]);
 PrintArray(matrix);
 FillArray(matrix);
 PrintArray(matrix);
-if ( m > matrix.Length(0) - 1 || n > matrix.Length(1) )
-{
-    Console.Write(" Такой позиции нет");
-}
-Console.Write($"{matrix[m, n]} \t");
+ReleaseMatrix(matrix, m, n);
+
